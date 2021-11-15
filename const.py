180aee3abd6f21ac03f2ef04d90ece9fe2284aa5
Lucia Murzová <55753060+LuciaMurzova@users.noreply.github.com>
2021-11-15 13:09:07 +0100

    asi ok
    možno som aj konečne trafila tabu search + vykreslenie cesty na konci

--- const.py
+++ const.py
@@ -1,4 +1,12 @@
+import sys
 
 POCET_MIEST = 20
 
 VELKOST_PLOCHY = 200
+
+VELKOST_TABU = 20
+
+POCET_GENERACII = 150
+
+N_TICE = 3  # pocet potomkov vybranych do jedneho turnaja
+MAX_INT = sys.maxsize
