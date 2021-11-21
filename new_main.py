4a572d22d1caa96dec15500b6a825766cc0bfd52
Lucia Murzová <55753060+LuciaMurzova@users.noreply.github.com>
2021-11-21 17:02:09 +0100

    pred prerobenim pridavania do tabu

--- new_main.py
+++ new_main.py
@@ -13,9 +13,9 @@ class Cesta:
 
     def __init__(self, zoznam, dlzka: float):
         self.zoznam_miest = zoznam
-        self.dlzka_cesty = self.vypocitaj_dlzku()
+        self.dlzka_cesty = dlzka
 
-    def vypocitaj_dlzku(self) -> float:
+    def vypocitaj_dlzku(self):
         dlzka: float = 0
         # prejde vsetky mesta
         for mesto in range(const.POCET_MIEST - 1):
@@ -24,7 +24,7 @@ class Cesta:
             dlzka += np.linalg.norm(mesto1 - mesto2)
 
         # doratanie cesty z posledneho mesta do prveho
-        mesto1 = np.array(polohy_miest[self.zoznam_miest[mesto + 1]])
+        mesto1 = np.array(polohy_miest[self.zoznam_miest[const.POCET_MIEST - 1]])
         mesto2 = np.array(polohy_miest[self.zoznam_miest[0]])
         dlzka += np.linalg.norm(mesto1 - mesto2)
         self.dlzka_cesty = dlzka
@@ -32,27 +32,12 @@ class Cesta:
 
 polohy_miest = [[0 for i in range(2)] for j in range(const.POCET_MIEST)]    # nacitane/vygenerovane polohy
 najkratsia_cesta: Cesta
-cislo_generacie: int = 0
+cislo_iteracie: int = 0
 
 tabu_list = []
 susedia = []            # novy susedia aktualne najkratesej cesty
-najdene_hodnoty = []    # vybrane cesty z kazdej generacie - pre vykreslenie grafu hladania
-
-
-#def vypocitaj_dlzku(stav):
-#    dlzka: int = 0
-#    # prejde vsetky mesta
-#    for mesto in range(const.POCET_MIEST-1):
-#        mesto1 = np.array(polohy_miest[stav[mesto]])
-#        mesto2 = np.array(polohy_miest[stav[mesto + 1]])
-#        dlzka += np.linalg.norm(mesto1 - mesto2)
-
-    # doratanie cesty z posledneho mesta do prveho
-#    mesto1 = np.array(polohy_miest[stav[mesto+1]])
-#    mesto2 = np.array(polohy_miest[stav[0]])
-#    dlzka += np.linalg.norm(mesto1 - mesto2)
-
-#    return dlzka
+najdene_hodnoty = []    # vybrane cesty z kazdej iteracie - pre vykreslenie grafu hladania
+iteracie = []          # ukladanie cisiel iteracii pre nasledne vykreslenie grafu
 
 
 def swap(rodic, pozicia_prve: int, pozicia_druhe: int):
@@ -64,60 +49,56 @@ def swap(rodic, pozicia_prve: int, pozicia_druhe: int):
 
 
 def vygeneruj_mesta():
-    global najkratsia_cesta, polohy_miest, zaciatocna_cesta
+    global najkratsia_cesta, polohy_miest
     zoznam_miest = []
 
     # vygeneruje nahodne polohy miest podla zadaneho poctu a velkosti mapy
     for mesto in range(const.POCET_MIEST):
-        polohy_miest[mesto][0] = random.randint(1, const.VELKOST_PLOCHY)
-        polohy_miest[mesto][1] = random.randint(1, const.VELKOST_PLOCHY)
+        polohy_miest[mesto][0] = random.randint(0, const.VELKOST_PLOCHY)
+        polohy_miest[mesto][1] = random.randint(0, const.VELKOST_PLOCHY)
         print(polohy_miest[mesto][0], polohy_miest[mesto][1])
         zoznam_miest.append(mesto)
 
     najkratsia_cesta = Cesta(np.array(zoznam_miest), 0)
-#    najkratsia_cesta.dlzka_cesty = vypocitaj_dlzku(najkratsia_cesta.zoznam_miest)
-    #najkratsia_cesta.vypocitaj_dlzku()
-    zaciatocna_cesta = najkratsia_cesta.dlzka_cesty
+    najkratsia_cesta.vypocitaj_dlzku()
 
     print("Zaciatocna dlzka: %.2f" % najkratsia_cesta.dlzka_cesty)
 
 
 def nacitaj_mesta():
-    global najkratsia_cesta, polohy_miest, zaciatocna_cesta
+    global najkratsia_cesta, polohy_miest
     zoznam_miest = []
 
-    # vygeneruje nahodne polohy miest podla zadaneho poctu a velkosti mapy
+    # vytvori zaciatocny zoznam miest 0 - const.POCET_MIEST-1
     for mesto in range(const.POCET_MIEST):
     #    polohy_miest[mesto][0] = int(input())
     #    polohy_miest[mesto][1] = int(input())
-    #    print(polohy_miest[mesto][0], polohy_miest[mesto][1])
         zoznam_miest.append(mesto)
 
-    polohy_miest = np.loadtxt('vstup.txt', delimiter=' ', dtype=int)
+    # nacita polohy miest zo vstupneho suboru
+    polohy_miest = np.loadtxt(const.VSTUPNY_SUBOR, delimiter=' ', dtype=int)
 
     najkratsia_cesta = Cesta(np.array(zoznam_miest), 0)
-   # najkratsia_cesta.dlzka_cesty = vypocitaj_dlzku(najkratsia_cesta.zoznam_miest)
-    #najkratsia_cesta.vypocitaj_dlzku()
-    zaciatocna_cesta = najkratsia_cesta.dlzka_cesty
+    najkratsia_cesta.vypocitaj_dlzku()
 
     print("Zaciatocna dlzka: %.2f" % najkratsia_cesta.dlzka_cesty)
 
 
 def novy_susedia(rodic):
-    global cislo_generacie, susedia
+    global cislo_iteracie, susedia, iteracie
 
     # kazde mesto vymeni s kazdym dalsim
     for mesto in range(const.POCET_MIEST):
         for permutacia in range(mesto+1, const.POCET_MIEST):
             nova_cesta = swap(rodic, permutacia, mesto)
-            #nova_cesta.dlzka_cesty = vypocitaj_dlzku(nova_cesta.zoznam_miest)
-           # nova_cesta.vypocitaj_dlzku()
+            nova_cesta.vypocitaj_dlzku()
             susedia.append(nova_cesta)
 
-    cislo_generacie += 1
+    cislo_iteracie += 1
+    iteracie.append(cislo_iteracie)
 
 
-def je_tabu(stav):
+def je_tabu(stav: []):
     global tabu_list
     for tabu_stav in tabu_list:
         prorovanenie = tabu_stav.zoznam_miest == stav.zoznam_miest
@@ -129,16 +110,13 @@ def je_tabu(stav):
 def skontroluj_najkratsiu():
     global tabu_list, najkratsia_cesta, susedia
     najlepsia: Cesta = Cesta(najkratsia_cesta.zoznam_miest, najkratsia_cesta.dlzka_cesty)
-    druha_najelpsia: Cesta = Cesta([], const.MAX_INT)
+    druha_najlepsia: Cesta = Cesta([], const.MAX_INT)
 
     for potomok in susedia:
-        if je_tabu(potomok) is False and najlepsia.dlzka_cesty < potomok.dlzka_cesty < druha_najelpsia.dlzka_cesty:
-            druha_najelpsia = potomok
+        if je_tabu(potomok) is False and najlepsia.dlzka_cesty < potomok.dlzka_cesty < druha_najlepsia.dlzka_cesty:
+            druha_najlepsia = potomok
 
         if je_tabu(potomok) is False and potomok.dlzka_cesty < najlepsia.dlzka_cesty:
-            #najkratsia_cesta.dlzka_cesty = potomok.dlzka_cesty
-            #najkratsia_cesta.zoznam_miest = potomok.zoznam_miest
-            #najkratsia_cesta.fitness = potomok.fitness
             najlepsia = potomok
 
     if najlepsia.dlzka_cesty != najkratsia_cesta.dlzka_cesty:
@@ -146,57 +124,38 @@ def skontroluj_najkratsiu():
         tabu_list.append(najlepsia)
         najdene_hodnoty.append(najlepsia.dlzka_cesty)
     else:
-        najkratsia_cesta = druha_najelpsia
-        tabu_list.append(druha_najelpsia)
-        najdene_hodnoty.append(druha_najelpsia.dlzka_cesty)
+        najkratsia_cesta = druha_najlepsia
+        tabu_list.append(druha_najlepsia)
+        najdene_hodnoty.append(druha_najlepsia.dlzka_cesty)
     if len(tabu_list) >= const.VELKOST_TABU:
         tabu_list.pop(0)
 
 
-def nakresli_cestu(stav):
+def nakresli_cestu(stav_na_kreslenie: []):
     root = Tk()
     canvas = Canvas(root, width=2 * const.VELKOST_PLOCHY + 20, height=2 * const.VELKOST_PLOCHY + 20)
     canvas.configure(background="black")
     canvas.pack()
 
     for mesto in range(const.POCET_MIEST-1):
-        x = polohy_miest[stav[mesto]][0] * 2
-        y = polohy_miest[stav[mesto]][1] * 2
-        Label(canvas, text=stav[mesto], bg="green").place(x=x-5, y=y-5)
+        mesto_x = polohy_miest[stav_na_kreslenie[mesto]][0] * 2
+        mesto_y = polohy_miest[stav_na_kreslenie[mesto]][1] * 2
+        Label(canvas, text=stav_na_kreslenie[mesto], bg="green").place(x=mesto_x - 5, y=mesto_y - 5)
 
-        b = np.array(polohy_miest[stav[mesto + 1]])
-        canvas.create_line(x, y, 2*b[0], 2*b[1], fill="blue")
+        dalsie_mesto = np.array(polohy_miest[stav_na_kreslenie[mesto + 1]])
+        canvas.create_line(mesto_x, mesto_y, 2*dalsie_mesto[0], 2*dalsie_mesto[1], fill="blue")
 
-    mesto += 1
-    x = polohy_miest[stav[mesto]][0] * 2
-    y = polohy_miest[stav[mesto]][1] * 2
-    Label(canvas, text=stav[mesto], bg="green").place(x=x-5, y=y-5)
+    mesto_x = polohy_miest[stav_na_kreslenie[const.POCET_MIEST - 1]][0] * 2
+    mesto_y = polohy_miest[stav_na_kreslenie[const.POCET_MIEST - 1]][1] * 2
+    Label(canvas, text=stav_na_kreslenie[const.POCET_MIEST - 1], bg="green").place(x=mesto_x - 5, y=mesto_y - 5)
 
-    b = np.array(polohy_miest[stav[0]])
-    canvas.create_line(x, y, 2*b[0], 2*b[1], fill="blue")
+    dalsie_mesto = np.array(polohy_miest[stav_na_kreslenie[0]])
+    canvas.create_line(mesto_x, mesto_y, 2*dalsie_mesto[0], 2*dalsie_mesto[1], fill="blue")
     canvas.mainloop()
 
 
-def maximum(pole):
-    max = 0
-    for cislo in pole:
-        if cislo > max:
-            max = cislo
-
-    return max
-
-
-def najdi_v_poli(hladane, pole):
-    for cislo in pole:
-        if hladane == cislo:
-            return True
-
-    return False
-
-
 if __name__ == '__main__':
-
-    if const.POCET_MIEST == 1:
+    if const.POCET_MIEST <= 1:
         exit("Neexistuje cesta")
     elif const.POCET_MIEST == 2 or const.POCET_MIEST == 3:
         exit("Existuje iba 1 cesta")
@@ -204,18 +163,14 @@ if __name__ == '__main__':
     #vygeneruj_mesta()
     nacitaj_mesta()
 
-    while cislo_generacie < const.POCET_GENERACII:
+    while cislo_iteracie < const.POCET_ITERACII:
         novy_susedia(najkratsia_cesta)
         # kontrola vytvorenych potomkov, ci nie je nova najkratsia
-        print("___", cislo_generacie, len(susedia), najkratsia_cesta)
+        print("___", cislo_iteracie, len(susedia), najkratsia_cesta)
 
         # prejde potomkov a vyberie najelpsieho - dalej bude generovat jeho susedov
         skontroluj_najkratsiu()
         susedia.clear()
-
-    print("\nnajkratsia ", najkratsia_cesta)
-    print('\ntabu ', tabu_list)
-    print("Generacie ", cislo_generacie)
 
     for stav in tabu_list:
         if stav.dlzka_cesty < najkratsia_cesta.dlzka_cesty:
@@ -223,16 +178,11 @@ if __name__ == '__main__':
 
     print("\nnajkratsia ", najkratsia_cesta)
     print('\ntabu ', tabu_list)
-    print("Generacie ", cislo_generacie)
+    print("Iteracie ", cislo_iteracie)
 
-    x = []
-
-    for g in range(cislo_generacie):
-        x.append(g)
-
-    plt.xlabel('Generacie')
+    plt.xlabel('Iteracie')
     plt.ylabel('Dlzka cesty')
-    plt.plot(x, najdene_hodnoty)
+    plt.plot(iteracie, najdene_hodnoty)
     plt.show()
 
     nakresli_cestu(najkratsia_cesta.zoznam_miest)
